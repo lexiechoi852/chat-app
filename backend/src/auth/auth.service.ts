6b691c764aa6e2c7b2b9c23e6b736e448a5a74ef
Lexie Choi <lexiechoi@ricacorp.com>
2022-04-19 11:08:11 +0800

    Removed email from JWT payload

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
   }
 
   async login(user) {
-    const payload = { email: user.email, sub: user.id };
+    const payload = { sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
