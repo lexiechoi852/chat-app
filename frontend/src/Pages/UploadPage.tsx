75390338476f81576be5c8cf1ed0129caa4faa94
Lexington Choi <lexingtonc852@gmail.com>
2022-05-11 17:57:19 +0800

    Added profile picture preview and reset function in upload page

--- frontend/src/Pages/UploadPage.tsx
+++ frontend/src/Pages/UploadPage.tsx
@@ -1,25 +1,46 @@
-import { Box, Button, FormControl, FormLabel, HStack, Input, VStack, Image } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
+import { CloseIcon } from '@chakra-ui/icons';
+import { Box, Button, FormControl, FormLabel, HStack, Input, VStack, Image, InputGroup, InputRightElement } from '@chakra-ui/react'
+import React, { useEffect, useRef, useState } from 'react'
 
 export default function UploadPage() {
-  const [image, setImage] = useState('default-single-icon.svg');
-  const [preview, setPreview] = useState('');
+  const [image, setImage] = useState<File | undefined>(undefined);
+  const [preview, setPreview] = useState<string>('default-single-icon.svg');
+  const ref = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    
+    if (!image) {
+      reset();
+      return;
+    }
+
+    const imageUrl = URL.createObjectURL(image);
+    setPreview(imageUrl)
   
     return () => {
-      
+      URL.revokeObjectURL(imageUrl);
     }
-  }, [])
+  }, [image])
   
-  const handleImage = (image: any) => {
+  const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      reset();
+      return;
+    }
 
+    setImage(e.target.files[0]);
   };
 
   const upload = () => {
+    
+  };
 
-  }
+  const reset = () => {
+    if (ref && ref.current && ref.current) {
+      ref.current.value = '';
+    }
+    setImage(undefined);
+    setPreview('default-single-icon.svg');
+  };
 
   return (
     <Box
@@ -33,17 +54,36 @@ export default function UploadPage() {
       <VStack w='full' p={8}>
         <Image
           boxSize='100px'
-          src={image}
+          src={preview}
         />
         <FormControl mb={2}>
             <FormLabel>Upload profile picture</FormLabel>
-            <Input type='file' p={1.5} accept='image/*'  />
+            <InputGroup>
+              <Input 
+                p={1.5} 
+                type='file' 
+                accept='image/*' 
+                ref={ref}
+                onChange={handleImage}
+              />
+              <InputRightElement>
+                <Button onClick={reset}>
+                  <CloseIcon />
+                </Button>
+              </InputRightElement>
+            </InputGroup>
           </FormControl>
           <HStack w='full' justifyContent='end'>
             <Button rounded='none' w={['full', 'auto']} alignSelf='end'>
               Skip
             </Button>
-            <Button rounded='none' colorScheme='blue' w={['full', 'auto']} alignSelf='end' onClick={()=>upload()}>
+            <Button
+              rounded='none' 
+              colorScheme='blue' 
+              w={['full', 'auto']} 
+              alignSelf='end' 
+              onClick={()=>upload()}
+            >
               Upload
             </Button>
           </HStack>
