a9fa66a443ae09b9cf097c8cf3feab8d69d7c919
Lexington Choi <lexingtonc852@gmail.com>
2022-06-05 21:51:48 +0800

    Added input field in chat box

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -1,13 +1,18 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
-import { useAppSelector } from '../hooks';
+import { Box, FormControl, HStack, Image, Input } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../hooks';
 import { Chat } from '../store/chatsSlice';
 import { User } from '../store/usersSlice';
 
 export default function ChatBox() {
+  // const socket = useRef<any>();
+  const [messageContent, setMessageContent] = useState('');
 
   const { currentChat } =  useAppSelector((state) => state.chats);
   const { user } =  useAppSelector((state) => state.auth);
+  const { messages, isLoading } =  useAppSelector((state) => state.messages);
+
+  const dispatch = useAppDispatch();
 
   const handleChatName = (currentUser: User | undefined, chat: Chat) => {
     if (!currentUser) {
@@ -23,6 +28,25 @@ export default function ChatBox() {
     }
   }
   
+  useEffect(() => {
+    
+   
+  }, [])
+  
+  const sendMessage = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'Enter' && messageContent && currentChat) {
+      const newMessage = {
+        content: messageContent,
+        chatId: currentChat._id
+      }
+    }
+  }
+
+  const handleTyping = (e :React.ChangeEvent<HTMLInputElement>) => {
+
+  }
+
+  
   return (
     <Box 
       display={{ base: currentChat ? 'flex' : 'none', md: 'flex'}}
@@ -36,9 +60,24 @@ export default function ChatBox() {
       {
           currentChat 
           ? (
-            <Box>
-                {currentChat.isGroupChat ? currentChat.chatName : handleChatName(user, currentChat)}
-            </Box> 
+            <HStack w='full' justifyContent='start'>
+              <Image 
+                boxSize='40px' 
+                src={currentChat.isGroupChat 
+                  ? 'default-group-icon.svg' 
+                  : 'default-single-icon.svg'}
+              />
+              <Box>
+                  {currentChat.isGroupChat ? currentChat.chatName : handleChatName(user, currentChat)}
+              </Box>
+              <FormControl onKeyDown={(e) => sendMessage(e)}>
+                <Input
+                  placeholder='Send a message'
+                  value={messageContent}
+                  onChange={handleTyping}
+                />
+              </FormControl>
+            </HStack>
           ) : (
           <Box>Default Chat</Box>
         )
