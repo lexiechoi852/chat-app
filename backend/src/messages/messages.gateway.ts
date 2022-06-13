2e3087e650e4d1ec10d3fb9e3308fcad47bae6bc
Lexington Choi <lexingtonc852@gmail.com>
2022-06-13 08:41:18 +0800

    Fixed bug and added debugging code

--- backend/src/messages/messages.gateway.ts
+++ backend/src/messages/messages.gateway.ts
@@ -4,24 +4,51 @@ import {
   SubscribeMessage,
   MessageBody,
   ConnectedSocket,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { Param, Request, UseGuards } from '@nestjs/common';
+import { Param, Request, UseGuards, Logger } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
-    orignin: '*',
+    origin: '*',
   },
 })
 @UseGuards(JwtAuthGuard)
-export class MessagesGateway {
+export class MessagesGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server: Server;
 
+  private logger: Logger = new Logger('MessagesGateway');
+
   constructor(private readonly messagesService: MessagesService) {}
+
+  afterInit() {
+    this.logger.log('Messages Gateway Initialized');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+    this.logger.debug(
+      `No. of connected socket ${this.server.engine.clientsCount}`,
+    );
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  // @SubscribeMessage('msgToServer')
+  // handleMessage(client: Socket, text: string) {
+  //   return { event: 'msgToClient', data: text };
+  // }
 
   @SubscribeMessage('createMessage')
   async create(
