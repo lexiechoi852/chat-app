c2184767de29f3fb3828a03e333bee98c73e61bd
Lexington Choi <lexingtonc852@gmail.com>
2022-06-27 17:10:28 +0800

    feat: added debounce search

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,18 +1,44 @@
 import { SearchIcon } from '@chakra-ui/icons'
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
-import React from 'react'
+import React, { useRef } from 'react'
+import { useAppDispatch, useAppSelector } from '../hooks'
+import { searchUsers } from '../store/usersThunk'
+import { debounce } from 'lodash-es'
 
 interface SearchProps {
     mode: string
 }
 
 export default function Search({ mode }: SearchProps) {
+
+  const dispatch = useAppDispatch();
+  const { searchText } =  useAppSelector((state) => state.users);
+
+  const handleSearch = (search: string) => {
+    if (!search || searchText === search) {
+      return;
+    }
+    console.log(search, 'search')
+    
+    dispatch(searchUsers(search));
+  }
+
+  const debouncedSearch = useRef(
+    debounce(async (criteria) => {
+      await handleSearch(criteria);
+    }, 400)
+  ).current;
+
   return (
     <InputGroup>
         <InputLeftElement>
             <SearchIcon />
         </InputLeftElement>
-        <Input variant='filled' placeholder='Search' />
+        <Input
+          variant='filled'
+          placeholder='Search' 
+          onChange={(e) => debouncedSearch(e.target.value)}
+        />
   </InputGroup>
   )
 }
